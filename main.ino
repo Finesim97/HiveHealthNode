1033e88ce3d6aa78bd0facd5293d928faf91dbe7
Lukas Jansen <lukas.jansen1997@yahoo.de>
2018-12-04 14:02:41 +0100

    Added example sketch and build script

---
+++ main.ino
@@ -0,0 +1,16 @@
+/*
+For ESP32 board support follow this document:
+https://github.com/espressif/arduino-esp32/blob/master/docs/arduino-ide/boards_manager.md
+*/
+
+#include <Arduino.h>
+
+void setup() {
+  Serial.begin(9600);
+
+}
+
+void loop() {
+  Serial.println("I am alive!");
+  delay(1000);
+}
